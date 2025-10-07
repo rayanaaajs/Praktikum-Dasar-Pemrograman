b0d967ae10b283a9354d329407558fb1cdd60687
rayanaaajs <rayanajakasurya54@gmail.com>
2025-10-07 14:13:24 +0700

    percobaan 3

---
+++ jobsheet5/nestedUjianSkripsi26.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class nestedUjianSkripsi26 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String pesan;
+
+        System.out.println("apakah mahasigma sudah bebas kompen (ya/tidak)? ");
+        String bebasKompen = sc.nextLine(). trim();
+
+        System.out.println("masukkan jumlah log bimbingan pembimbing 1: ");
+        int bimbinganP1 = sc.nextInt();
+        System.out.println("masukkan jumlah log bimbingan pembimbing 2: ");
+        int bimbinganP2 = sc.nextInt();
+
+    if (bebasKompen.equalsIgnoreCase("Ya")) {
+        if (bimbinganP1 >= 8 && bimbinganP2 >= 4) {
+            pesan = "Semua syarat terpenuhi. Mahasiswa boleh mendaftar ujian skripsi";
+        } else if (bimbinganP1 < 8 && bimbinganP2 < 4) {
+            pesan = "Gagal! Log bimbingan P1 kurang dari 8 kali dan P2 kurang dari 4 kali";
+        } else if (bimbinganP1 < 8) {
+            pesan = "Gagal! Log bimbingan P1 belum mencapai 8 kali";
+        } else {
+            pesan = "Gagal! Log bimbingan P2 belum mencapai 4 kali";
+        }
+    } else {
+        pesan = "Gagal! Mahasiswa masih memiliki tanggungan kompen";
+    }
+        System.out.println(pesan);
+
+
+        sc.close();
+    }
+}
+
