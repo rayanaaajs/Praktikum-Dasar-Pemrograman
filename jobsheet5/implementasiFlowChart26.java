94a1565c594f8fcf2f74e9632b2af140f006aafb
rayanaaajs <rayanajakasurya54@gmail.com>
2025-10-11 09:38:41 +0700

    Tugas 3 Akses Wifi

--- jobsheet5/implementasiFlowChart26.java
+++ jobsheet5/implementasiFlowChart26.java
@@ -1,24 +1,32 @@
 import java.util.Scanner;
+
 public class implementasiFlowChart26 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-public static void main(String[] args) {
-    Scanner input= new Scanner(System.in);
+        System.out.println("masukkan nama pengguna (dosen/mahasiswa)");
+        String namaPengguna = input.nextLine();
 
-    String bawaKTM, regisOnline;
+        if (namaPengguna.equalsIgnoreCase("dosen")) {
+            System.out.println("Akses WiFi diberikan (dosen)");  
+        } 
+            else if (namaPengguna.equalsIgnoreCase("mahasiswa")) {
+                System.out.println("masukkan jumlah SKS");
+                int jumlahSKS = input.nextInt();
 
-    System.out.println("Apakah anda membawa KTM? (ya/tidak)");
-    bawaKTM = input.nextLine();
-    System.out.println("Apakah anda registrasi online? (ya/tidak)");
-    regisOnline = input.nextLine();
+                if (jumlahSKS >= 12) {
+                System.out.println("Akses WiFi diberikan (mahasiswa aktif)");
+                } else {
+                System.out.println("Akses ditolak, SKS kurang dari 12");
+                }
+            }
+        else {
+            System.out.println("Akses ditolak");
+        }
 
-
-    if (bawaKTM.equalsIgnoreCase("ya") || regisOnline.equalsIgnoreCase("ya")) {
-        System.out.println("Boleh masuk");
-    } else {
-        System.out.println("Tidak boleh masuk");
+                
+        input.close();
     }
-       
-    input.close();
-}
+
+ }
     
-}
