affe41916a78eab4b81f5e14b61770aeed2dc9f8
rayanaaajs <rayanajakasurya54@gmail.com>
2025-10-21 15:54:01 +0700

    .

---
+++ jobsheet7/Tugas2Looping.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Tugas2Looping {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jenisKendaraan, durasiParkir, totalHarga = 0;
+        System.out.println("masukkan jenis kendaraan (1.Mobil, 2.Motor, 0.keluar): ");
+        jenisKendaraan = sc.nextInt();
+
+    
+    }
+}
