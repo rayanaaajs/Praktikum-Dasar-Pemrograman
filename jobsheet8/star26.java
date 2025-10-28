bea8c101413679f1002b576f3858ca9c157627e8
rayanaaajs <rayanajakasurya54@gmail.com>
2025-10-28 15:02:28 +0700

    .

---
+++ jobsheet8/star26.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class star26 {
+    public static void main(String[] args) {
+        
+         Scanner sc = new Scanner(System.in);
+
+         System.out.print("masukkan nilai n = ");
+         int n = sc.nextInt();
+
+         for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+
+
+         }
+    }
+}
