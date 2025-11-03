6bff1fc220dfe14fc5101ac625d0b3be2bba343d
rayanaaajs <rayanajakasurya54@gmail.com>
2025-11-03 12:49:09 +0700

    .

---
+++ jobsheet8/square26.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+    public class square26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("masukkan nilai n = ");
+        int N = sc.nextInt();
+
+        for (int iOuter = 1; iOuter <= N; iOuter++){
+            for (int i = 1; i <= N; i++){
+                System.out.print("*"); 
+
+            }
+            System.out.println();
+        }   
+    sc.close();
+    }
+}
