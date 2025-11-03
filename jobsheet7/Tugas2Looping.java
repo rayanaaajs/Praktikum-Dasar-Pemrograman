cf51b426564acdb7e00c3e861038bd8de426b79d
rayanaaajs <rayanajakasurya54@gmail.com>
2025-11-03 08:14:47 +0700

    .

--- jobsheet7/Tugas2Looping.java
+++ jobsheet7/Tugas2Looping.java
@@ -4,9 +4,24 @@ public class Tugas2Looping {
         Scanner sc = new Scanner(System.in);
 
         int jenisKendaraan, durasiParkir, totalHarga = 0;
-        System.out.println("masukkan jenis kendaraan (1.Mobil, 2.Motor, 0.keluar): ");
-        jenisKendaraan = sc.nextInt();
 
-    
+          do {
+             System.out.print("Masukan jenis kendaraan (1.mobil 2. Motor 0. keluar): ");
+             jenisKendaraan = sc.nextInt();
+                if (jenisKendaraan==1||jenisKendaraan==2){
+                System.out.print("Masukan Durasi parkir anda: ");
+                durasiParkir = sc.nextInt();
+                if (durasiParkir>=5){
+                    totalHarga+=12500;
+                }else if (jenisKendaraan==1){
+                    totalHarga+=durasiParkir*3000;
+                }else if (jenisKendaraan==2){
+                    totalHarga+=durasiParkir*2000;
+                }
+                System.out.println("Harga parkir yang harus dibayarkan: "+totalHarga);
+            } 
+        } while(jenisKendaraan!=0);
+
+        sc.close();
     }
 }
