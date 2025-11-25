366b58a0bd308c28ed998ce1b53f15f545633670
rayanaaajs <rayanajakasurya54@gmail.com>
2025-11-25 14:30:09 +0700

    fungsi

---
+++ jobsheet12/Kafe26.java
@@ -0,0 +1,19 @@
+package jobsheet12;
+
+public class Kafe26 {
+    public static void main(String[] args) {
+        Menu();
+    }
+        public static void Menu() {
+            System.out.println("==== MENU RESTO KAFE ====");
+            System.out.println("1. Kopi Hitam - 15,000");
+            System.out.println("2. capucino - 20,000");
+            System.out.println("3. latte - 22,000");
+            System.out.println("4. teh tarik - 12,000");
+            System.out.println("5. roti bakar - 10,000");
+            System.out.println("6. mie goreng - 18,000");
+            System.out.println("=========================");
+            System.out.println("silahkan pilih menu yang anda inginkan");
+        }
+    }
+
